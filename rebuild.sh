1ea35aeb6af80eb91927a97510a17077d83ccca4
wyf <wyf3634@163.com>
2021-08-11 11:56:39 +0800

    修改脚本

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,10 @@ hexo generate
 echo "编译完成====》》》"
 
 git add public
+git add db.json
 git commit -m 'rebuild'
 git push
 echo "提交完成====》》》"
+
+echo "正在打开Gitee Pages 》》》"
+open https://gitee.com/im-fan/im-fan/pages
