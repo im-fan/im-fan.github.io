9ef020e0945fb16ac4b85011c93792cf978a8429
weiyf <weiyf3634@163.com>
2022-05-27 13:57:42 +0800

    update config

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@ hexo clean
 hexo generate
 echo "编译完成====》》》"
 
-git add public
+git add docs
 git add db.json
 git commit -m 'rebuild'
 sleep 2
@@ -10,4 +10,4 @@ git push
 echo "提交完成====》》》"
 
 echo "正在打开GitHub Pages 》》》"
-open https://github.com/im-fan/im-fan.github.io/settings/pages
+open https://github.com/im-fan/im-fan.github.io/actions
